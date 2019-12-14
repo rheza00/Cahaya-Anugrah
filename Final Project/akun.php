3bc81e952d02ab3dc3b4122db5bd8eac567cfafc
rheza00 <41385987+rheza00@users.noreply.github.com>
2019-12-14 10:23:03 +0700

    Update akun.php

--- Final Project/akun.php
+++ Final Project/akun.php
@@ -6,14 +6,7 @@ $result = mysqli_query($db, $query);
 $customer = mysqli_fetch_array($result);
 ?>
 <?php session_start();?>
-<!-- 
-adsad
 
-asdad
-
-
-
-adafqrsdfsfsdf -->
 <?php if($customer['gambar']!=null):?>
 <img width="200" src="<?php echo $customer['gambar']?>"><br />
 <?php echo ' Wellcome ' . $_SESSION['User'];?><br />
